46d831bb94ca1798e5facbf25372b69c5627bea7
DucVanGD <168454226+DucVanGD@users.noreply.github.com>
2024-10-25 01:14:37 +0700

    Bat debug

--- src/app.py
+++ src/app.py
@@ -56,4 +56,4 @@ if __name__ == '__main__':
         db.create_all()
         app.config['cartOption'].delete_guest_cart()
     
-   # app.run(debug= True, port= 5000)
+    app.run(debug= True, port= 5000)
